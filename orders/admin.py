e832af86235f5338c8bc4ec823e9aab6705f8da0
glauvus <glauvus@hotmail.com>
2020-10-21 18:30:51 +0300

    added register and menu pages and modified the templates

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Size, Topping, Pizza
+from .models import Size, Topping, Pizza, Pasta, Salad, Dessert, Drink
 
 # Register your models here.
 admin.site.register(Size)
 admin.site.register(Topping)
 admin.site.register(Pizza)
+admin.site.register(Pasta)
+admin.site.register(Salad)
+admin.site.register(Dessert)
+admin.site.register(Drink)
--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from .models import Pasta, Salad, Dessert, Drink
 
 
 # Create your views here.
 def index(request):
     if request.user.is_authenticated:
-        return render(request, "orders/index.html", context={"user": request.user})
-    return render(request, "orders/index.html", context={"user": None})
+        return render(request, "orders/index.html", {"user": request.user})
+    return render(request, "orders/index.html")
 
 
 def login_view(request):
@@ -27,4 +29,22 @@ def logout_view(request):
 
 
 def register_view(request):
-    return render(request, "orders/register.html", context={"user": None})
+    form = RegisterForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse("index"))
+    return render(request, "orders/register.html", {"form": RegisterForm()})
+
+
+def menu_view(request, menu_cat):
+    if menu_cat == 'pasta':
+        items = Pasta.objects.all()
+    elif menu_cat == 'salads':
+        items = Salad.objects.all()
+    elif menu_cat == 'desserts':
+        items = Dessert.objects.all()
+    elif menu_cat == 'drinks':
+        items = Drink.objects.all()
+    else:
+        return HttpResponseRedirect(reverse("index"))
+    return render(request, "orders/menu.html", {"items": items, "menu_cat": menu_cat})
